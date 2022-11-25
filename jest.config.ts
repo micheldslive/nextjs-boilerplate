f5726f392660d446444aa8cdc7140b97b991e22a
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-25 15:31:54 -0300

    chore(setup/deps): adding jest

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.ts'],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+const config = createJestConfig(customJestConfig)
+
+export default config
